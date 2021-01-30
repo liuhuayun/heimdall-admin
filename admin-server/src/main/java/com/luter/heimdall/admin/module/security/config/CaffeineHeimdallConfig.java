85f0864c378b5f35f9ac2ec916b813b301d28781
luter <lutao.cao@139.com>
2021-01-30 10:45:24 +0800

    修改 readme

--- admin-server/src/main/java/com/luter/heimdall/admin/module/security/config/CaffeineHeimdallConfig.java
+++ admin-server/src/main/java/com/luter/heimdall/admin/module/security/config/CaffeineHeimdallConfig.java
@@ -22,41 +22,27 @@
 
 package com.luter.heimdall.admin.module.security.config;
 
-import com.luter.heimdall.cache.redis.authorization.RedisAuthorizationMetaDataDao;
-import com.luter.heimdall.cache.redis.limiter.RedisLoginPasswordRetryLimitImpl;
-import com.luter.heimdall.cache.redis.session.RedisSessionDaoImpl;
-import com.luter.heimdall.core.authorization.authority.GrantedAuthority;
+import com.luter.heimdall.cache.caffeine.CaffeineAuthorizationMetaDataDao;
+import com.luter.heimdall.cache.caffeine.CaffeineLoginPasswordRetryLimitImpl;
+import com.luter.heimdall.cache.caffeine.CaffeineSessionDaoImpl;
 import com.luter.heimdall.core.authorization.dao.AuthorizationMetaDataCacheDao;
 import com.luter.heimdall.core.authorization.service.AuthorizationMetaDataService;
 import com.luter.heimdall.core.manager.AuthenticationManager;
 import com.luter.heimdall.core.manager.AuthorizationManager;
 import com.luter.heimdall.core.manager.limiter.LoginPasswordRetryLimit;
 import com.luter.heimdall.core.servlet.ServletHolder;
-import com.luter.heimdall.core.session.SimpleSession;
 import com.luter.heimdall.core.session.dao.SessionDAO;
 import com.luter.heimdall.core.session.scheduler.DefaultInvalidSessionClearScheduler;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-
-import java.util.Collection;
-import java.util.List;
 
 @Configuration
 @Slf4j
-@RequiredArgsConstructor
-public class RedisHeimdallConfig {
-    private final RedisTemplate<String, SimpleSession> sessionRedisTemplate;
-    private final StringRedisTemplate stringRedisTemplate;
-    private final RedisTemplate<String, Collection<String>> sysAuthRedisTemplate;
-    private final RedisTemplate<String, List<? extends GrantedAuthority>> userAuthRedisTemplate;
-
+public class CaffeineHeimdallConfig {
     @Bean
     public LoginPasswordRetryLimit loginPasswordRetryLimit() {
-        final RedisLoginPasswordRetryLimitImpl loginPasswordRetryLimit = new RedisLoginPasswordRetryLimitImpl(stringRedisTemplate);
+        final CaffeineLoginPasswordRetryLimitImpl loginPasswordRetryLimit = new CaffeineLoginPasswordRetryLimitImpl();
         loginPasswordRetryLimit.setAttemptLimit(5);
         loginPasswordRetryLimit.setLockedDuration(60);
         return loginPasswordRetryLimit;
@@ -65,7 +51,7 @@ public class RedisHeimdallConfig {
     @Bean
     public SessionDAO sessionDAO(ServletHolder servletHolder) {
         log.warn("初始化 SessionDAO");
-        return new RedisSessionDaoImpl(sessionRedisTemplate, stringRedisTemplate, userAuthRedisTemplate, servletHolder);
+        return new CaffeineSessionDaoImpl(servletHolder);
     }
 
     @Bean
@@ -77,7 +63,7 @@ public class RedisHeimdallConfig {
     @Bean
     public AuthorizationMetaDataCacheDao authorizationMetaDataCacheDao() {
         log.warn("初始化 系统权限数据 MetaDataDao");
-        return new RedisAuthorizationMetaDataDao(sysAuthRedisTemplate);
+        return new CaffeineAuthorizationMetaDataDao();
     }
 
     @Bean
